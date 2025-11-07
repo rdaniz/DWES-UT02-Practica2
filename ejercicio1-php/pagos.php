9e7da31aef30015c0367467b98585c62c2dec37a
rdaniz <dani_pmz@hotmail.com>
2025-11-07 17:47:15 +0100

    Creación de html/php con bucles foreach y tablas

--- ejercicio1-php/pagos.php
+++ ejercicio1-php/pagos.php
@@ -42,8 +42,8 @@ Array multidimensional:
 // Array asociativo multidimensional $socios
 $socios = [
     // Nuevo array asociatico multidimensional [socio1] dentro de $socios
+    // id único socio1
     'socio1' => [
-        'id' => 's1',
         'nombre' => 'Daniel',
         'apellidos' => 'Ponz Martínez',
         'dni' => '11111111V',
@@ -128,7 +128,6 @@ $socios = [
     ],
 
     'socio2' => [
-        'id' => 's2',
         'nombre' => 'Juan',
         'apellidos' => 'Sánchez Burriel',
         'dni' => '22222222B',
@@ -224,7 +223,7 @@ $socio = $socios['socio1'];
 // Inicializar la variable totalPagado a 0 donde se sumará todo lo que esté en estado Pagado
 $totalPagado = 0;
 // Bucle foreach que recorrerá todos los pagos del socio y almacenándolos en la variable $pago temporalmente
-// en cada iteración
+// en cada iteración y sumará los importes
 foreach ($socio['pagos'] as $pago) {
     // Condición de si el estado es Pagado, suma el importe a $totalPagado
     if ($pago['estado'] === 'Pagado') {
@@ -232,6 +231,94 @@ foreach ($socio['pagos'] as $pago) {
     }
 }
 
-// Prueba mostrar por consola un $totalPagado
+// Prueba mostrar por consola un $totalPagado - en consola php pagos.php
 // echo $totalPagado;
 ?>
+
+<!-- Creación documento html -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>dponzmv pagos.php</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 30px;
+            font-size: 20px;
+        }
+        table {
+            border-collapse: collapse;
+        }
+        th, td {
+            border: 1px solid;
+            text-align: center;
+            padding: 8px;
+        }
+        th {
+            background-color: #ddd;
+        }
+        .pendiente {
+            background-color: #feb7bdff;
+        }
+
+    </style>
+</head>
+<body>
+    <!-- Mostrar datos del socio seleccionado -->
+
+    <h1>Pagos de un socio</h1>
+    <h2>Datos del socio</h2>
+    <p><strong>Nombre: </strong><?php echo $socio['nombre'] ?></p>
+    <p><strong>Apellidos: </strong><?php echo $socio['apellidos'] ?></p>
+    <p><strong>DNI: </strong><?php echo $socio['dni'] ?></p>
+    <p><strong>Email: </strong><?php echo $socio['email'] ?></p>
+    <p><strong>Teléfono: </strong><?php echo $socio['telefono'] ?></p>
+
+    <h2>Detalle de los pagos</h2>
+
+    <!-- Creación de tabla -->
+
+    <table>
+        <tr>
+            <th>Mes</th>
+            <th>Importe</th>
+            <th>Estado</th>
+            <th>Fecha de pago</th>
+        </tr>
+        <tr>
+            <!-- Recorrer todos los estados de pago
+                - Si son Pendiente usa la clase pendiente (fondo rojo)
+                - Inserta mes, importe y estado de cada pago
+            -->
+            <?php foreach ($socio['pagos'] as $pago) { ?>
+                <?php if ($pago['estado'] === 'Pendiente') { ?>
+                    <tr class="pendiente">
+                <?php } else { ?>
+                    <tr>
+                <?php } ?>
+                    <td><?php echo $pago['mes']; ?></td>
+                    <td><?php echo $pago['importe']; ?><span> €</span></td>
+                    <td><?php echo $pago['estado']; ?></td>
+                    <td>
+                        <?php
+                        // Si existe fecha de pago, la inserta. Si es nula pone un -
+                        if ($pago['fecha_pago'] !== null) {
+                            echo $pago['fecha_pago'];
+                        } else {
+                            echo '-';
+                        }
+                        ?>
+                    </td>
+                </tr>
+            <?php } ?>
+            <tr>
+                <td><strong>Total pagado</strong></td>
+                <!-- Mostar la variable totalPagado calculada previamente -->
+                <td><?php echo $totalPagado ?><span> €</span></td>
+            </tr>
+    </table>
+</body>
+</html>
